6aadc068d4fe8795eb73fe2b5cabff22af449b0f
Zordius Chen <zordius@yahoo-inc.com>
2013-02-04 19:12:00 +0800

    fix travis script

--- travis/before.sh
+++ travis/before.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 YUIV=yui_3.8.1
+GETYUI=0
 echo "Before Script: `pwd`"
 
 cd "$(dirname "$0")"
@@ -9,15 +10,17 @@ cd ../;
 wait
 
 if [ -n "$TRAVIS" ]; then
-echo "Installing Yogi"
+    echo "Installing Yogi"
     npm -g install yogi -loglevel silent
+    if [ "1" -eq "GETYUI" ]; then
+        echo "get last yui3 files for testing";
+        wget http://yui.zenfs.com/releases/yui3/$YUIV.zip
+        unzip $YUIV.zip
+        mv yui/build build
+    fi
     cd src
     echo "building entire library with yogi";
     yogi build --no-lint --cache
-    echo "get last yui3 files for testing";
-    wget http://yui.zenfs.com/releases/yui3/$YUIV.zip
-    unzip $YUIV.zip
-    mv yui/build/* build
 else
     echo "no make....";
 fi
