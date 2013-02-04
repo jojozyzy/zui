052d2eadae0c0ac5eb734e71021fca62184213d6
Zordius Chen <zordius@yahoo-inc.com>
2013-02-04 20:35:19 +0800

    fix for bad directories which cause yogi test wrong

--- travis/before.sh
+++ travis/before.sh
@@ -12,15 +12,16 @@ wait
 if [ -n "$TRAVIS" ]; then
     echo "Installing Yogi"
     npm -g install yogi -loglevel silent
-    if [ "1" -eq "$GETYUI" ]; then
-        echo "get last yui3 files for testing";
-        wget http://yui.zenfs.com/releases/yui3/$YUIV.zip
-        unzip $YUIV.zip
-        mv yui/build build
-    fi
     cd src
     echo "building entire library with yogi";
     yogi build --no-lint --cache
+    cd ..
+    if [ "1" -eq "$GETYUI" ]; then
+        echo "get last yui3 files for testing";
+        wget http://yui.zenfs.com/releases/yui3/$YUIV.zip
+        unzip $YUIV.zip yui/build/*
+        mv yui/build/* build
+    fi
 else
     echo "no make....";
 fi
