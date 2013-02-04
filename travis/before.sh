3e2dec1f2cc3d79d5f7ae8574def1d11fc0db9fb
Zordius Chen <zordius@yahoo-inc.com>
2013-02-04 19:02:38 +0800

    now build depend on yui built files

--- travis/before.sh
+++ travis/before.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+YUIV=yui_3.8.1
 echo "Before Script: `pwd`"
 
 cd "$(dirname "$0")"
@@ -14,6 +14,10 @@ echo "Installing Yogi"
     cd src
     echo "building entire library with yogi";
     yogi build --no-lint --cache
+    echo "get last yui3 files for testing";
+    wget http://yui.zenfs.com/releases/yui3/$YUIV.zip
+    unzip $YUIV.zip
+    mv yui/build/* build
 else
     echo "no make....";
 fi
